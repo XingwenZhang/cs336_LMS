3b00c5708c4a6aa98b98538da07ba8bdc03845ae
Xingwen <zhangxingwen0428@outlook.com>
2026-07-31 00:30:19 -0700

    fix

--- assignment1/model/attention.py
+++ assignment1/model/attention.py
@@ -10,8 +10,11 @@ def scaled_dot_product_attention(query: torch.Tensor, key: torch.Tensor, value: 
     # mask: (..., query_length, key_length)
 
     d_k = query.size(-1)
-    match = einsum('...ij, ...kj -> ...ik', query, key)
-    masked_match = match.masked_fill(~mask, float('-inf'))
-    normalized_match = softmax(masked_match/math.sqrt(d_k), -1)
+    match = einsum('...ij, ...kj -> ...ik', query, key)/math.sqrt(d_k)
+    if mask is not None:
+        masked_match = match.masked_fill(~mask, float('-inf'))
+    else:
+        masked_match = match
+    normalized_match = softmax(masked_match, -1)
     attention = einsum('...ik, ...kj -> ...ij', normalized_match, value)
     return attention
